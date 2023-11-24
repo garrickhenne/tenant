8c0556bbf84b79e34be870855e168bb0b717b4f6
Garrick Henne <garrickhenne@outlook.com>
2023-11-24 00:16:54 -0800

    Create server init file with basic configuration and ability to connect to mongodb

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  // Connect to Mongo.
+  // TODO: Maybe move this connection somewhere else and add options to the connect method to specify database: test or prod.
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.p7l79i6.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+      console.log(`Server is Fire at http://localhost:${port}`);
+    })
+    .catch(error => console.log(error));
+});
