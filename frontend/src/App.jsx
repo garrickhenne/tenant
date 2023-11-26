8f79800983636da2641f6ff019d215bfeb91a6b8
Garrick Henne <garrickhenne@outlook.com>
2023-11-26 13:28:30 -0800

    Install tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* Tailwind */}
+        <h1 className='text-3xl font-bold underline'>
+          Hello world!
+        </h1>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
