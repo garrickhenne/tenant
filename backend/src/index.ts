da45cfb93b6d4f18899554e1c7d371b8d038fcf1
Garrick Henne <garrickhenne@outlook.com>
2023-11-26 20:54:45 -0800

    Add /signup and /logout endpoints, use morgan middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,8 @@ import express, { Express, Request, Response, Application } from 'express';
 import { connectToMongoDB } from './db/connection';
 import dotenv from 'dotenv';
 import cookieSession from 'cookie-session';
-import { loginUser } from './controllers/UserController';
+import { loginUser, signUpUser } from './controllers/UserController';
+import morgan from 'morgan';
 
 //routes
 const dashboardRouter = require('./routes/dashboardRouter');
@@ -18,6 +19,7 @@ app.use(cookieSession({
   name: 'session',
   keys: [COOKIE_SESSION_KEY]
 }));
+app.use(morgan('dev'));
 
 const port = process.env.PORT || 8000;
 
@@ -39,6 +41,11 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.post('/login', (req, res) => loginUser(req, res));
+app.post('/signup', (req, res) => signUpUser(req, res));
+app.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/');
+});
 
 app.listen(port, () => {
 
