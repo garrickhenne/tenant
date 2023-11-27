f699ba93bd8885e8ad0b3086a021fdf87ed90172
Garrick Henne <garrickhenne@outlook.com>
2023-11-26 20:01:00 -0800

    use middleware for json and urlencoding

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,27 @@ import express, { Express, Request, Response, Application } from 'express';
 import { connectToMongoDB } from './db/connection';
 import dotenv from 'dotenv';
 import cookieSession from 'cookie-session';
+import { loginUser } from './controllers/UserController';
 
 //For env File
 dotenv.config();
 const COOKIE_SESSION_KEY: string = process.env.COOKIE_SESSION_KEY || 'key1';
 
 const app: Application = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieSession({
   name: 'session',
   keys: [COOKIE_SESSION_KEY]
 }));
+
 const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
+
+app.post('/login', (req, res) => loginUser(req, res));
 
 app.listen(port, () => {
 
