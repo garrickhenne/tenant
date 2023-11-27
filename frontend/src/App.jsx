bab9760a2083913616da169f0c9a3158d97219bd
Garrick Henne <garrickhenne@outlook.com>
2023-11-27 10:37:46 -0800

    Remove vite components/styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,12 +15,6 @@ const App = () => {
         <h1 className='text-3xl font-bold underline'>
           Hello world!
         </h1>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
