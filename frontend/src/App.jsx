d6723bec4bf71b671a9e924e0ecda3152948649c
Garrick Henne <garrickhenne@outlook.com>
2023-11-27 14:45:39 -0800

    Move App homepage content to 'Home' component and have App serve as Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './routes/Signup';
+import Home from './routes/Home';
+import Login from './routes/Login';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
-      <div>
-        {/* Tailwind */}
-        <h1 className='text-3xl font-bold underline'>
-          Hello world!
-        </h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </>
   );
 };
