e6ec4f33241d5ac1e0e7febb3dab7d20a04463e3
Garrick Henne <garrickhenne@outlook.com>
2023-11-26 18:39:34 -0800

    Add unique check for emails schema

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -9,7 +9,7 @@ export interface IUser {
 
 const userSchema = new Schema<IUser>({
   username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   dateCreated: { type: Date, default: new Date(), immutable: true }
 });
