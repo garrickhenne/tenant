440aeb8dac6d7ee4846e90cba19d567a2cb8bac1
Robert Shum <robertshum@gmail.com>
2023-12-02 04:19:22 -0800

    add icons and btn f(n).

--- frontend/src/components/Review/Score.jsx
+++ frontend/src/components/Review/Score.jsx
@@ -2,15 +2,36 @@ import HealthIcon from "./icons/Health";
 
 const Score = function(props) {
 
-  if (props.text === 'Sentiment') {
+  const maxScore = props.maxScore;
 
+  const IconComponent = props.icon;
+
+  let displayScore = `${props.score}/${maxScore}`;
+  let percentage = `${(props.score / maxScore) * 100}%`;
+  if (props.text === 'Sentiment') {
+    displayScore = Math.round(((props.score + 1) / 2) * 100);
+    if (displayScore > 0 && displayScore < 25) {
+      displayScore = 'Scorned';
+    }
+    if (displayScore >= 25 && displayScore < 40) {
+      displayScore = 'Disliked';
+    }
+    if (displayScore >= 40 && displayScore < 60) {
+      displayScore = 'Neutral';
+    }
+    if (displayScore >= 60 && displayScore < 70) {
+      displayScore = 'Liked';
+    }
+    if (displayScore >= 71) {
+      displayScore = 'Revered';
+    }
+    percentage = `${displayScore}%`;
   }
 
-  const percentage = `${(props.score / 5) * 100}%`;
   console.log(props.text, percentage);
   return (
     <div class="flex items-center py-3">
-      <HealthIcon></HealthIcon>
+      <IconComponent />
       <div class="space-y-3 flex-1">
         <div class="flex items-center">
           <h4
@@ -19,7 +40,7 @@ const Score = function(props) {
             {props.text}
           </h4>
           <span class="px-2 py-1 rounded-lg bg-red-50 text-blue-700 text-xs">
-            {props.score}/5
+            {displayScore}
           </span>
         </div>
         <div class="overflow-hidden bg-blue-50 h-1.5 rounded-full w-full">
--- frontend/src/components/Review/test.jsx
+++ frontend/src/components/Review/test.jsx
@@ -1,5 +1,11 @@
 import Score from './Score';
 import { useNavigate } from 'react-router-dom';
+import HealthIcon from './icons/Health';
+import RespectIcon from './icons/Respect';
+import RepairIcon from './icons/Repair';
+import SentimentIcon from './icons/Sentiment';
+import OverallScoreIcon from './icons/OverallScore';
+import EditIcon from './icons/Edit';
 
 const Test = function(props) {
 
@@ -18,6 +24,11 @@ const Test = function(props) {
 
   const reviewId = item._id;
 
+  const overallScore = 50;
+
+  const categoryMaxScore = 5;
+  const overallMaxScore = 100;
+
   const handleSubmit = (event) => {
     event.preventDefault();
     navigate(`/reviews/${reviewId}`);
@@ -27,26 +38,45 @@ const Test = function(props) {
     <section
       className="w-full p-6 rounded-lg max-w-2xl shadow-lg shadow-gray-300 bg-white"
     >
-      <header className="flex items-center mb-1">
+      <header className="flex justify-between items-center mb-1">
 
         <h3 className="font-medium text-lg">"{reviewTitle}"</h3>
-
+        <EditIcon onClick={handleSubmit}></EditIcon>
       </header>
       <h3 className="font-medium text-lg mb-2">{landlord.lastName}, {landlord.firstName}</h3>
       <p className="text-left whitespace-normal ">
         {reviewDesc}
       </p>
       <section className="py-4 grid grid-cols-2 gap-x-6">
-        <Score text="Health and Safety" score={healthSafety}></Score>
-        <Score text="Respect" score={respect}></Score>
-        <Score text="Repair" score={repair}></Score>
-        <Score text="Sentiment" score={sentiment}></Score>
-
+        <Score
+          icon={HealthIcon}
+          text="Health and Safety"
+          score={healthSafety}
+          maxScore={categoryMaxScore}>
+        </Score>
+        <Score
+          icon={RespectIcon}
+          text="Respect"
+          score={respect}
+          maxScore={categoryMaxScore}>
+        </Score>
+        <Score
+          icon={RepairIcon}
+          text="Repair"
+          score={repair}
+          maxScore={categoryMaxScore}>
+        </Score>
+        <Score
+          icon={SentimentIcon}
+          text="Sentiment"
+          score={sentiment}
+          maxScore={categoryMaxScore}>
+        </Score>
       </section>
 
       {/* Overall Score */}
       <footer className="border-t border-gray-100 pt-4">
-        <Score text="Overall Score"></Score>
+        <Score icon={OverallScoreIcon} text="Overall Score" score={overallScore} maxScore={overallMaxScore}></Score>
       </footer>
     </section>
   );
