a7aea2b71bad71c90f10def0e143a43bd8425838
Garrick Henne <garrickhenne@outlook.com>
2023-12-02 01:46:22 -0800

    Update helper to average all ratings + sentiment

--- frontend/src/helpers/LandlordHelpers.js
+++ frontend/src/helpers/LandlordHelpers.js
@@ -2,10 +2,14 @@ export const calculateAverageRatings = (reviews) => {
   const repairsAverage = reviews.reduce((prev, curr) => prev + curr.repair, 0) / reviews.length;
   const respectAverage = reviews.reduce((prev, curr) => prev + curr.respect, 0) / reviews.length;
   const safetyAverage = reviews.reduce((prev, curr) => prev + curr.healthSafety, 0) / reviews.length;
+  const overall = (repairsAverage + respectAverage + safetyAverage) / 3;
+  const sentiment = reviews.reduce((prev, curr) => prev + curr.sentiment, 0) / reviews.length;
 
   return {
     repair: repairsAverage,
     respect: respectAverage,
-    safety: safetyAverage
+    safety: safetyAverage,
+    overall,
+    sentiment
   };
 };
