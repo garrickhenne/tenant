ee91640af8f46346258d38a7f0b446bb6a882885
Garrick Henne <garrickhenne@outlook.com>
2023-12-02 16:55:09 -0800

    Add review component and icons from other branch

--- frontend/src/components/LandlordView/LandlordReview.jsx
+++ frontend/src/components/LandlordView/LandlordReview.jsx
@@ -1,23 +1,81 @@
-import MiniSentimentGauge from "./MiniSentimentGauge";
-import ReadOnlyStarRating from "./ReadOnlyStarRating";
+import Score from '../Review/Score';
+import HealthIcon from '../Review/icons/Health';
+import RespectIcon from '../Review/icons/Respect';
+import RepairIcon from '../Review/icons/Repair';
+import SentimentIcon from '../Review/icons/Sentiment';
+import OverallScoreIcon from '../Review/icons/OverallScore';
+import {
+  getSentimentText,
+  getSentimentPercentage,
+  getDisplayScore,
+  getOverallDisplayScore,
+  getPercentage,
+  getOverallScorePercentage
+} from '../../helpers/ComponentHelper';
 
-const LandlordReview = ({ review }) => {
+const Review = function({ item, isLandlordView }) {
+
+  const reviewTitle = item.title;
+  const reviewDesc = item.description;
+  const sentiment = item.sentiment;
+  const healthSafety = item.healthSafety;
+  const respect = item.respect;
+  const repair = item.repair;
+  const overallScore = item.overallScore;
+
   return (
-    <article className="flex flex-col gap-y-4 border-2 border-white rounded-2xl p-5 text-start basis-[45%]">
-      <h2 className="text-4xl">{ review.title }</h2>
-      <div className="flex flex-col gap-y-2">
-        <ReadOnlyStarRating  value={review.healthSafety} label='Health and Safety' />
-        <ReadOnlyStarRating  value={review.repair} label='Repair' />
-        <ReadOnlyStarRating  value={review.respect} label='Respect' />
-        <div className="flex flex-row gap-x-3">
-          <p>Sentiment:</p>
-          <MiniSentimentGauge value={review.sentiment} />
-        </div>
-      </div>
-      { review.description } <br />
-    </article>
+    <section
+      className={`w-full p-6 border-2 border-white rounded-lg max-w-2xl shadow-md shadow-gray-500 basis-[45%] bg-transparent`}
+    >
+      <header className="flex justify-between items-center mb-1">
+        {/* Title Review + Edit Button */}
+        <h3 className="font-medium text-lg">&ldquo;{reviewTitle}&rdquo;</h3>
+      </header>
 
+      {/* Review */}
+      <p className="text-left whitespace-normal italic">
+        {reviewDesc}
+      </p>
+
+      {/* Score attributes */}
+      <section className="py-4 grid grid-cols-2 gap-x-6">
+        <Score
+          icon={HealthIcon}
+          text="Health and Safety"
+          displayScore={getDisplayScore(healthSafety)}
+          percentage={getPercentage(healthSafety)}>
+        </Score>
+        <Score
+          icon={RespectIcon}
+          text="Respect"
+          displayScore={getDisplayScore(respect)}
+          percentage={getPercentage(respect)}>
+        </Score>
+        <Score
+          icon={RepairIcon}
+          text="Repair"
+          displayScore={getDisplayScore(repair)}
+          percentage={getPercentage(repair)}>
+        </Score>
+        <Score
+          icon={SentimentIcon}
+          text="Sentiment"
+          displayScore={getSentimentText(sentiment)}
+          percentage={getSentimentPercentage(sentiment)}>
+        </Score>
+      </section>
+
+      {/* Overall Score */}
+      <footer className="border-t border-gray-100 pt-4">
+        <Score
+          icon={OverallScoreIcon}
+          text="Overall Score"
+          displayScore={getOverallDisplayScore(overallScore)}
+          percentage={getOverallScorePercentage(overallScore)}>
+        </Score>
+      </footer>
+    </section>
   );
 };
 
-export default LandlordReview;
+export default Review;
