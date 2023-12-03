fc1627cc5a5718a295df2541f12879465880045b
Garrick Henne <garrickhenne@outlook.com>
2023-12-02 16:55:20 -0800

    Update review list

--- frontend/src/components/LandlordView/ReviewsList.jsx
+++ frontend/src/components/LandlordView/ReviewsList.jsx
@@ -4,7 +4,7 @@ const ReviewsList = ({ reviews }) => {
   return (
     <ul className="flex flex-row gap-y-10 gap-x-5 flex-wrap justify-evenly">
       { reviews.map(review => (
-        <LandlordReview review={ review } key={ review._id } />
+        <LandlordReview item={ review } key={ review._id } />
       ))}
     </ul>
   );
