577a8f0c145209f03b171da2eb2cd6f70c2c3497
Daniel Tan <danieltan380@gmail.com>
2023-12-02 21:40:29 -0800

    Add styling and axios call with search query.

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import '../styling/scrollBar.css';
 
 export const SearchBar = () => {
+  const navigation = useNavigate();
 
   const [searchQuery, setSearchQuery] = useState('');
 
@@ -9,20 +13,40 @@ export const SearchBar = () => {
   const fetchSearchResults = (e) => {
     const newSearchQuery = e.target.value;
     setSearchQuery(newSearchQuery);
-    setResults([newSearchQuery]);
+    console.log(results);
   };
+
+  const searchLandlord = (name) => {
+    const params = {name};
+    axios.get('/api/search', {params})
+      .then((response) => response.data)
+      .then((results) => {
+        setResults([...results]);
+      });
+  };
+  
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    searchLandlord(searchQuery);
+  }, [searchQuery]);
 
   return <div className="input-wrapper flex flex-col items-center space-y-16">
     <input
-      className="pl-4 pr-20 w-3/4 text-left focus:outline-none bg-transparent border-solid border-2 border-white rounded-full h-16"
+      className="pl-4 pr-20 w-3/4 text-left text-xl focus:outline-none bg-transparent border-solid border-2 border-white rounded-full h-16"
       placeholder="🔎 Search by name..."
       value={searchQuery}
       onChange={fetchSearchResults}
     />
 
-    {searchQuery && <div className="w-3/4 max-h-72 text-left bg-transparent border-solid border-2 border-white rounded-full">
-      {results.map((result, id) => {
-        return <div onClick={() => console.log(result)} className="pl-4 pr-4 hover:bg-cyan-600 rounded-full" key={id}>{result}</div>;
+    {searchQuery && <div className="w-3/4 text-left bg-transparent border-solid border-2 border-white max-h-96 overflow-y-auto rounded-3xl scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-500 scrollbar-thumb-rounded-full">
+      {results.map((result) => {
+        return <div
+          onClick={() => navigation(`/landlords/${result.landlord._id}`)}
+          className="pl-4 pt-1 pb-1 pr-4 text-2xl hover:bg-cyan-600 rounded-full"
+          key={result.landlord._id}>{`${result.landlord.firstName} ${result.landlord.lastName}`}
+        </div>;
       })}
     </div>}
   </div>;
