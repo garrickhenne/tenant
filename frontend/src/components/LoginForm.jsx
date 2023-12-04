17aad89ff00c015d20d073573a4581cd6fe341b3
Daniel Tan <danieltan380@gmail.com>
2023-12-04 13:10:09 -0800

    Change text of labels to white.

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -31,7 +31,7 @@ const LoginForm = () => {
   return(
     <form className="flex flex-col items-center py-10 space-y-4" onSubmit={ handleSubmit }>
       <div id="login__email" className="flex flex-col w-1/2 max-w-[33%]">
-        <label className="text-left ml-3 mb-1">Email</label>
+        <label className="text-left ml-3 mb-1 text-slate-200">Email</label>
         <input
           type="email"
           className="pl-4 bg-transparent border-solid border-2 border-white rounded-full h-11"
@@ -40,7 +40,7 @@ const LoginForm = () => {
         />
       </div>
       <div id="login__password" className="flex flex-col w-1/2 max-w-[33%]">
-        <label className="text-left ml-3 mb-1">Password</label>
+        <label className="text-left ml-3 mb-1 text-slate-200">Password</label>
         <input
           type="password"
           className="pl-4 bg-transparent border-solid border-2 border-white rounded-full h-11"
@@ -48,7 +48,7 @@ const LoginForm = () => {
           onChange={ handlePasswordChange }
         />
       </div>
-      <button type="submit" className="min-w-[18%] bg-transparent border-solid border-2 border-white rounded-full">Login</button>
+      <button type="submit" className="min-w-[18%] bg-transparent border-solid border-2 border-white rounded-full text-slate-200">Login</button>
     </form>
   );
 };
