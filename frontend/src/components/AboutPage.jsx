73ad91cce7da18704119287c3cc5ff57be28a86b
Daniel Tan <danieltan380@gmail.com>
2023-12-05 18:18:03 -0800

    Adjusted some lines to match final presentation document.

--- frontend/src/components/AboutPage.jsx
+++ frontend/src/components/AboutPage.jsx
@@ -5,10 +5,7 @@ const AboutPage = () => {
       <article className="text-left">
         <ul className="px-24 text-3xl">
           <li className="pb-5 list-disc">
-            As tenants, background checks are performed. Tenants should know about what it is like to live under that landlord.
-          </li>
-          <li className="pb-5 list-disc">
-            First viewing can hide many unexpected things that come up during daily living.
+            We thought that Tenants should be aware of past living experiences of landlords to inspire confidence in renters.
           </li>
           <li className="list-disc">
             We wanted to create an app to inform prospective tenants in their renting decisions by displaying reviews about living experiences. They can also create new reviews.
@@ -62,12 +59,16 @@ const AboutPage = () => {
           <li className="pb-5 list-disc">
             Frontend: JS, React, Framer-Motion, Tailwind.
           </li>
+
+          <li className="indent-7 pb-6">
+            Framer-Motion for animations, Mapbox for map function, and Tailwind for rapid UI development.
+          </li>
           
           <li className="list-disc pb-3">
-            Backend:Express, NodeJS, Mongoose.
+            Backend:Express, NodeJS, Mongoose, Google Natural Language
           </li>
 
-          <li className="indent-7 pb-5">
+          <li className="indent-7 pb-6">
             TypeScript: Allowed us to create interfaces for our backend workflow to help distinguish modules from one another.
           </li>
 
@@ -103,11 +104,8 @@ const AboutPage = () => {
           <li className="indent-7 pb-2">
             Getting sentiment value from Google Natural Language.
           </li>
-          <li className="indent-7 pb-2">
-            Adding pins on MapBox was a surprising challenge.
-          </li>
           <li className="indent-7">
-            Working around hourly limit for Mapbox API calls.
+            Hitting the free tier threshold for Geonames API, we had to throttle ourselves.
           </li>
         </ul>
         <section className="justify-center items-center flex pt-10">
@@ -121,13 +119,12 @@ const AboutPage = () => {
       <header className="text-4xl p-10 font-bold">Teamwork</header>
       <article className="text-left flex flex-col">
         <ul className="text-3xl pl-24">
-          <li className="pb-5"> Introduced AGILE tendencies:</li>
-          <li className="list-disc pb-5"> Daily standups containing what we did yesterday, what we planned on doing today, and any blockers that we had encountered or would need help with.</li>
+          <li className="list-disc pb-5"> Introduced AGILE behaviour such as Daily standups containing what we did yesterday, what we planned on doing today, and any blockers that we had encountered.</li>
           <li className="list-disc pb-3"> During planning stage involved creating Figma wireframes of how we wanted the individual pages and forms to look.</li>
           <li className="indent-7 pb-2"> Figma allows us to work on the same screen at the same time.</li>
           <li className="list-disc pb-5"> Constant merge-calls on Discord to resolve merge conflicts / new features are on the latest codebase.</li>
-          <li className="list-disc pb-5"> Created a GitHub project with a Kanban board containing our user stories as well as further subtasks that each user story might need.</li>
-          <li className="list-disc pb-5"> Directly merging to the main branch was prohibited and all branches required a PR + review from another peer which helped facilitate knowledge of each other’s changes in addition to our own.</li>
+          <li className="list-disc pb-5"> Created a GitHub project with a Kanban board containing our user stories and subtasks for each.</li>
+          <li className="list-disc pb-5"> We worked with a pull request system with peer reviews, which helped facilitate knowledge of each other’s changes in addition to our own.</li>
         </ul>
       </article>
       <section className="flex flex-col items-center justify-center">
@@ -143,6 +140,19 @@ const AboutPage = () => {
         </img>
 
       </section>
+      <header className="text-4xl p-10 font-bold">Further Development</header>
+      <article className="text-left pb-5">
+        <ul className="text-3xl pl-24">
+          <li className="list-disc pb-5">Would like to implement an Incident Report feature, where users can report specific issues related to the property, such as plumbing and electrical.</li>
+          <li className="list-disc pb-5">Deploy the project live.</li>
+        </ul>
+      </article>
+      <header className="text-4xl pt-20 pb-10 font-bold italic items-center justify-center flex">Thanks for stopping by!</header>
+      <ul className="pb-10">
+        <a className="hover:underline px-10" href="https://github.com/garrickhenne">@garrickhenne</a>
+        <a className="hover:underline px-10" href="https://github.com/dantan380">@dantan380</a>
+        <a className="hover:underline px-10" href="https://github.com/robertshum">@robertshum</a>
+      </ul>
     </main>
   </div>;
 };
