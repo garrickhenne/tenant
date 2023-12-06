a34b4131459f4f0040fb4e2a255222beaed3e434
Garrick Henne <garrickhenne@outlook.com>
2023-12-06 01:13:15 -0800

    Edit Property model to support location points

--- backend/src/model/Property.ts
+++ backend/src/model/Property.ts
@@ -1,9 +1,17 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
 
+export interface ILocation {
+  type: string;
+  coordinates: number[];
+}
+
+// longitude comes first in a GeoJSON coordinate array, not latitude
+// [LAT, LONG]
 export interface IProperty extends Document {
   postalCode: string;
   streetName: string;
   streetNumber: number;
+  location: ILocation;
   landlordId: Types.ObjectId;
 }
 
@@ -11,6 +19,15 @@ const propertySchema = new Schema<IProperty>({
   postalCode: { type: String, required: true },
   streetName: { type: String, required: true },
   streetNumber: { type: Number, required: true },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+    }
+  },
   landlordId: { type: Schema.ObjectId, required: true, ref: "Landlord" }
 });
 
