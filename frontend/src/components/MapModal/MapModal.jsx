3000b69eda50ff3c774f4a160cc91d236f923cea
Garrick Henne <garrickhenne@outlook.com>
2023-12-06 01:25:50 -0800

    Remove throttle. Update map modal to support many landlords under one postal code. Make map bigger

--- frontend/src/components/MapModal/MapModal.jsx
+++ frontend/src/components/MapModal/MapModal.jsx
@@ -1,14 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import IconClose from "./icons/IconClose";
 
-const MapModal = ({ landlords, openModal, data, closeModal }) => {
+const MapModal = ({ landlords, closeModal }) => {
   const navigate = useNavigate();
 
   return (
     <main className='relative z-50' onClick={closeModal} >
       <div className='p-1 gap-y-2 absolute w-52 bg-white rounded-lg flex flex-col text-black overflow-y-auto'>
         <IconClose onClick={closeModal} />
-        <p className="text-start text-lg font-medium hover:bg-slate-200 min-w-full pl-3 transition-colors rounded-full" onClick={() => navigate(`/landlords/${data.landlordId}`)}>{data.firstName} {data.lastName}</p>
+        {landlords.map(landlord =>
+          <p
+            className="text-start text-lg font-medium hover:bg-slate-200 min-w-full pl-3 transition-colors rounded-full"
+            onClick={() => navigate(`/landlords/${landlord._id}`)}
+            key={landlord._id}
+          >
+            {landlord.firstName} {landlord.lastName}
+          </p>
+        )
+        }
       </div>
     </main>
   );
